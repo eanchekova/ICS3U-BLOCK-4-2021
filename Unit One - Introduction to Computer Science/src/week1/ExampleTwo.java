25ed0bbe63ad81eead8a1ee195e1c8b5cc245403
eanchekova <eanchekova@bayviewglen.ca>
2021-09-09 16:19:26 -0400

    commit

--- Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
+++ Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -2,7 +2,7 @@ package week1;
 
 public class ExampleTwo {
     public static void main(String[] args) {
-        System.out.println("A");
+        System.out.println("A!");
         System.out.print("B");
         System.out.print("C");
         System.out.println("D");
