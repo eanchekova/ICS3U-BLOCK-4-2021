7afb6803b39f4c8c64e4249960ed5947bf8fa4bb
eanchekova <eanchekova@bayviewglen.ca>
2021-09-13 14:32:39 -0400

    int/double

---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,25 @@
+package week2;
+
+public class ExampleSeven {
+
+    public static void main(String[] args) {
+final int x = 7;
+//x = 6; cannot reassign a value to a final variable
+
+
+
+final int y;
+y = 8;
+
+final int NUM_STUDENTS = 18;
+//y++ cannot modify the variable once it is set if it is constant
+
+
+
+
+
+
+
+    }
+    
+}
